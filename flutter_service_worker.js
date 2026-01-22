aa688291afbdf0967c163e15a4c20e52f80bb57c
addeenzml <addeenzml@gmail.com>
2026-01-22 15:43:26 +0800

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "87be5fd82f7bf8d116ccdc6c0024cbcf
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "45a98017361f3722611635a6ad5edab0",
+"flutter_bootstrap.js": "9f2e46fc31e5ebb95b0d041afc4228f8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
